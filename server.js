42b203a764f9c74a31ce948e2617eee4cde35cbf
Henry Mueller <muel0713@umn.edu>
2024-09-07 00:21:48 -0500

    start of front and back end

--- server.js
+++ server.js
@@ -6,3 +6,13 @@ app.set("views", "templates");
 app.set("view engine", "pug");
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use("/resources", express.static("resources"));
+
+// insert authentification here 
+
+// get the main page
+app.get("/", async (req, res) => {
+    res.render("");
+});
+
